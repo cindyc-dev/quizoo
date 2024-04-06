d0237f1587dd204647c458ce47e93c9dee5c7485
Xin Yu <chuaxc@unimelb.edu.au>
2024-04-06 17:55:06 +1100

    Add UseGameState Hook

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaArrowRightToBracket } from "react-icons/fa6";
 import PageLayout from "~/components/PageLayout";
-import RoomIdInput from "~/components/RoomIdInput";
 import { Button } from "~/components/ui/button";
+import { Indicator } from "~/components/ui/indicator";
+import useGameState, { initialGameState } from "~/hooks/useGameState";
 import { generateGamePin } from "~/utils/gamePin";
 
 export default function Create() {
-  const [gameId, setGameId] = useState("");
+  const [gameState, setGameState] = useGameState("gameState", initialGameState);
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    // TODO Check if currentGameId is still active - reset if no one is in the game
+  }, [])
+
   const handleCreate = () => {
     const newGameId = generateGamePin();
-    console.log({ newGameId });
-    setGameId(newGameId);
+    setGameState({
+      gameId: newGameId,
+    });
   };
 
   return (
     <PageLayout>
       <h1 className="text-primary-content">Create Game</h1>
-      {!gameId && (
+      {!gameState.gameId && (
         <Button
           type="submit"
           className="flex w-full max-w-sm justify-items-center gap-2 align-middle text-white"
@@ -29,7 +37,12 @@ export default function Create() {
           <FaArrowRightToBracket />
         </Button>
       )}
-      {gameId && <p>Game ID: {gameId}</p>}
+      {gameState.gameId && (
+        <div className="flex items-center justify-center gap-2">
+          <p>Game ID: {gameState.gameId}</p>
+          <Indicator variant={isOnline ? "online" : "offline"} />
+        </div>
+      )}
     </PageLayout>
   );
 }
